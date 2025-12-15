00ab22772574610a96925ed398cb09bc826b7d82
floriansalvi <floriansalvipro@gmail.com>
2025-12-15 09:06:10 +0100

    BaseImg component using NuxtImg

---
+++ components/Base/BaseImg.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+    src: {
+        type: String,
+        required: true
+    },
+    alt: {
+        type: String,
+        required: true
+    },
+    wrapperClass: {
+        type: String,
+        default: ""
+    },
+    imgClass: {
+        type: String,
+        default: ""
+    },
+    copyright: {
+        type: String,
+        default: ""
+    }
+});
+</script>
+
+<template>
+  <div class="" :class="wrapperClass">
+    <NuxtImg :src="src" :alt="alt" :class="imgClass"/>
+    <div v-if="copyright" class="">{{ copyright }}</div>
+  </div>
+</template>
