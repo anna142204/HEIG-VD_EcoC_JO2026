5bb69ce7a2aa74dfa48529b8f20221b14f530c1e
anna142204 <anna142204@gmail.com>
2025-12-15 16:07:28 +0100

    tailwind correction

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,29 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  components: true,
-  css: ['~/assets/css/variables.css'],
-  modules: [
-    "@nuxt/image",
-    "@nuxt/icon"
+
+  css: [
+    '~/assets/css/variables.css' 
   ],
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    ['@nuxt/icon', {
+        mode: 'svg', 
+        serverBundle: {
+          collections: ['lucide']
+        }
+    }],
+    ['@nuxt/image', {
+        format: ['webp'],
+        quality: 80,
+    }],
+  ],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
